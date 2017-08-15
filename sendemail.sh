ec2331dbb48e1c6f6151d6c67ebbc3f56581d785
andy12345638 <andy12345638@gmail.com>
2017-08-15 10:26:54 +0800

    send 2 png

--- sendemail.sh
+++ sendemail.sh
@@ -3,10 +3,11 @@ source email.conf
 
 DATE=`date +%Y-%m-%d:%H:%M:%S`
 Rscript temp120png.r
+Rscript temp10080png.r
 
 for i in "${emaillist[@]}"; do   # The quotes are necessary here
     #echo "$i"
-mutt -s "test mail" "$i" -a ./temp120png.png < mail.txt 
+mutt -s "test mail" "$i" -a ./temp120png.png -a ./temp10080png.png < mail.txt 
 done
 
 
